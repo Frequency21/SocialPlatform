02c80a7790d61edd4b86eb46c19c2b4c75e86a86
berberus <baloghetele285@gmail.com>
2021-04-13 18:46:59 +0200

    Felhasználó dummy adatok hozzáadva.

--- kevesbbeFancy.sql
+++ kevesbbeFancy.sql
@@ -14,6 +14,8 @@ DROP TABLE Meghivas CASCADE CONSTRAINTS;
 DROP TABLE Fenykepalbum CASCADE CONSTRAINTS;
 DROP TABLE Kategoria CASCADE CONSTRAINTS;
 DROP TABLE Fenykep CASCADE CONSTRAINTS;
+DROP TABLE Tagja CASCADE CONSTRAINTS;
+DROP TABLE Ismeros CASCADE CONSTRAINTS;
 DROP TYPE NEVTIPUS;
 DROP TYPE ERTEKELES;
 
@@ -43,9 +45,7 @@ CREATE TABLE Felhasznalo (
     szul_dat        DATE NOT NULL,
     munka_iskola    VARCHAR2(64),
     picture         BLOB,
-    hirfolyam_id    NUMBER NOT NULL,
-    isAdmin         NUMBER(1,0) NOT NULL,
-    CONSTRAINT fk_felhasznalo_hirfolyam FOREIGN KEY (hirfolyam_id) REFERENCES Hirfolyam(ID)
+    isAdmin         NUMBER(1,0) NOT NULL
 );
 /
 
@@ -55,8 +55,6 @@ CREATE TABLE Csoport(
     leiras          VARCHAR2(128),
     nev             VARCHAR2(16) NOT NULL,
     tulaj_id        NUMBER NOT NULL,
-    hirfolyam_id    NUMBER NOT NULL,
-    CONSTRAINT FK_Csoport_Hirfolyam FOREIGN KEY (hirfolyam_id) REFERENCES Hirfolyam(ID),
     CONSTRAINT FK_Csoport_Felhasznalo FOREIGN KEY (tulaj_id) REFERENCES Felhasznalo(ID)
 );
 /
@@ -90,7 +88,7 @@ CREATE TABLE Uzenet(
     /* Itt átneveztem a Felhasználó_id-t kuldo-re. */
     kuldo_id        NUMBER NOT NULL,
     cimzett_id      NUMBER NOT NULL,
-    szoveg          VARCHAR2(25),
+    szoveg          VARCHAR2(256),
     CONSTRAINT fk_uzenet_kuldo FOREIGN KEY (kuldo_id) REFERENCES Felhasznalo(ID),
     CONSTRAINT fk_uzenet_cimzett FOREIGN KEY (cimzett_id) REFERENCES Felhasznalo(ID)
 );
@@ -105,6 +103,20 @@ CREATE TABLE Meghivas(
     CONSTRAINT fk_meghivas_felhasznalo FOREIGN KEY (felhasznalo_id) REFERENCES Felhasznalo(ID),
     CONSTRAINT fk_meghivas_cimzett FOREIGN KEY (cimzett_id) REFERENCES Felhasznalo(ID),
     CONSTRAINT fk_meghivas_csoport FOREIGN KEY (csoport_id) REFERENCES Csoport(csoport_id)
+);
+/
+
+CREATE TABLE Tagja(
+    felhasznalo_id  NUMBER NOT NULL,
+    csoport_id      NUMBER NOT NULL,
+    CONSTRAINT fk_tagja_felhasznalo FOREIGN KEY (felhasznalo_id) REFERENCES Felhasznalo(ID),
+    CONSTRAINT fk_tagja_csoport FOREIGN KEY (csoport_id) REFERENCES Csoport(csoport_id)
+);
+/
+
+CREATE TABLE Ismeros(
+    felhasznalo1_id NUMBER NOT NULL,
+    felahsznalo2_id NUMBER NOT NULL
 );
 /
 
@@ -133,40 +145,81 @@ CREATE TABLE Fenykep(
 );
 /
 
-/*
-    felhasznalo_id  NUMBER NOT NULL,
-    cimzett_id      NUMBER NOT NULL,
-    csoport_id      NUMBER NOT NULL,
-    idopont         TIMESTAMP WITH LOCAL TIME ZONE NOT NULL,
-    isAccepted      NUMBER(1,0) NOT NULL,
-    CONSTRAINT fk_meghivas_felhasznalo FOREIGN KEY (felhasznalo_id) REFERENCES Felhasznalo(ID),
-    CONSTRAINT fk_meghivas_cimzett FOREIGN KEY (cimzett_id) REFERENCES Felhasznalo(ID),
-    CONSTRAINT fk_meghivas_csoport FOREIGN KEY (csoport_id) REFERENCES Csoport(csoport_id)
-*/
-
-/*Hirfolyam*/
-INSERT INTO Hirfolyam;
-INSERT INTO Hirfolyam;
-INSERT INTO Hirfolyam;
-INSERT INTO Hirfolyam;
-INSERT INTO Hirfolyam;
-INSERT INTO Hirfolyam;
-INSERT INTO Hirfolyam;
-INSERT INTO Hirfolyam;
-
 
 /*Felhasználók*/
-INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,hirfolyam_id,isAdmin)  VALUES('1q2w3e4r',NEVTIPUS('Balogh','Etele'),SYSTIMESTAMP,SYSTIMESTAMP,7,0);
-INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,hirfolyam_id,isAdmin)  VALUES('1q2w3e4r',NEVTIPUS('Kiss','Olivér'),SYSTIMESTAMP,SYSTIMESTAMP,8,1);
-INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,hirfolyam_id,isAdmin)  VALUES('1q2w3e4r',NEVTIPUS('Burza','Ralf'),SYSTIMESTAMP,SYSTIMESTAMP,9,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Balogh','Etele'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Kiss','Olivér'),SYSTIMESTAMP,SYSTIMESTAMP,1);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Burza','Ralf'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Anikó','Hampton'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Pál','Kozma'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Krisztián','Anderson'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('László','Jones'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('László','Dixon'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Katalin','Robin'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Anna','Jourdan'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Perry','Auger'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Claudine','Montoya'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Andrew','Paris'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Mária','Chauveau'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Csaba','Devaux'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Attila','Bonnet'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Pauline','Balog'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Sarah','Collins'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Katalin','Lengyel'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Daniel','Boucher'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Raymond','Collins'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Virág','Moulin'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Gabriel','Vaillant'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Lorraine','Wilson'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('József','Sellers'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Stephanie','Balázs'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Gilbert','Lemaitre'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Lajos','Schmitt'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('René','Masse'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Katalin','Clerc'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Róbert','Sanders'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Anaïs','Harvey'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Donna','Daugherty'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Adrienne','Jacquot'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Erzsébet','Arnaud'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Mandy','Aubert'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Paul','Voisin'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Georges','Gonzalez'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Alexandria','Nixon'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Marcell','Burton'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Tracy','Ramirez'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Virginie','Gonzalez'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Richárd','Bogdán'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Etelka','Szabó'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Daniel','Schmitt'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Stephen','Dawson'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Miklós','Szabó'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Noël','Breton'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Amber','Blanc'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Gábor','Rousseau'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Gergely','Mckenzie'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Austin','Bouchet'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Lili','Sanchez'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Jacques','Fleury'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Benjamin','Guillot'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('János','Herve'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('András','Farkas'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Jessica','Tóth'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Heather','Nagy'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Nicholas','Masse'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Teresa','Lopez'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Sabrina','Stevenson'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('Éric','Jackson'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+INSERT INTO Felhasznalo(jelszo,nev,csatl_dat,szul_dat,isAdmin) VALUES('1q2w3e4r',NEVTIPUS('James','Morris'),SYSTIMESTAMP,SYSTIMESTAMP,0);
+
 
 /*Csoportok*/
-INSERT INTO Csoport(leiras,nev,tulaj_id,hirfolyam_id) VALUES('Számítógép alkatrészek, adok-veszek','Hardverapró',2,1);
-INSERT INTO Csoport(leiras,nev,tulaj_id,hirfolyam_id) VALUES('Adatbázis alapú redszerek helyesírása','Adatb2',1,2);
-INSERT INTO Csoport(leiras,nev,tulaj_id,hirfolyam_id) VALUES('Cryptovaluták nyaggatása','Brave New World',3,3);
-INSERT INTO Csoport(leiras,nev,tulaj_id,hirfolyam_id) VALUES('SZTE legjobbjai, a jelszó meg: ******','SZTE4EVER',2,4);
-INSERT INTO Csoport(leiras,nev,tulaj_id,hirfolyam_id) VALUES('Lelkisegélyszolgálat az egyetemisták ért','Cryout for a hero',2,5);
-INSERT INTO Csoport(leiras,nev,tulaj_id,hirfolyam_id) VALUES('Brühühűűűűűű, még nagyon sok mindent kell legépelnem','Suffer 2gether',2,6);
+INSERT INTO Csoport(leiras,nev,tulaj_id) VALUES('Számítógép alkatrészek, adok-veszek','Hardverapró',2);
+INSERT INTO Csoport(leiras,nev,tulaj_id) VALUES('Adatbázis alapú redszerek helyesírása','Adatb2',1);
+INSERT INTO Csoport(leiras,nev,tulaj_id) VALUES('Cryptovaluták nyaggatása','Brave New World',3);
+INSERT INTO Csoport(leiras,nev,tulaj_id) VALUES('SZTE legjobbjai, a jelszó meg: ******','SZTE4EVER',2);
+INSERT INTO Csoport(leiras,nev,tulaj_id) VALUES('Lelkisegélyszolgálat az egyetemisták ért','Cryout a hero',2);
+INSERT INTO Csoport(leiras,nev,tulaj_id) VALUES('Brühühűűűűűű, még nagyon sok mindent kell legépelnem','Suffer 2gether',2);
 
 /*Meghívások*/
 INSERT INTO Meghivas(felhasznalo_id,cimzett_id,csoport_id,idopont,isAccepted) VALUES(1,2,1,SYSTIMESTAMP,0);
@@ -179,6 +232,8 @@ INSERT INTO Meghivas(felhasznalo_id,cimzett_id,csoport_id,idopont,isAccepted) VA
 
 /*Posztok*/
 
+
+/*Üzenetek*/
 
 COMMIT;
 /
